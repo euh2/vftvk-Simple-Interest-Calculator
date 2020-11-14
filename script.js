714d1a03140421e2d0e0a8dde9ee2811666aeb13
euh2 <tb@tobeon.net>
2020-11-14 16:19:11 +0100

    css styles + prepare js calc M  index.html M  script.js M  style.css @t

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+window.addEventListener('load', (event) => {
+  for(var i=1; i<=40; i++){
+      var select = document.getElementById("years");
+      var option = document.createElement("OPTION");
+      select.options.add(option);
+      option.text = i;
+      option.value = i;
+  }
+});
 function compute()
 {
     p = document.getElementById("principal").value;
