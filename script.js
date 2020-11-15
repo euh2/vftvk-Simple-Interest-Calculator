041cd84e425ab9a68170f9b590c187138ed69a73
euh2 <tb@tobeon.net>
2020-11-15 17:16:26 +0100

    css styles + basic js calc M  index.html M  script.js M  style.css

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// adapted from https://www.w3schools.com/howto/howto_js_rangeslider.asp
+function change_rate_value()
+{
+  var slider = document.getElementById("slider_rate");
+  var number = document.getElementById("number_rate");
+  // slider.onload = function() {
+  //   slider.value = number.value;
+  // }
+
+  // Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function() {
+    number.value = this.value;
+  }
+  number.oninput = function() {
+    slider.value = number.value;
+  }
+}
+
 window.addEventListener('load', (event) => {
   for(var i=1; i<=40; i++){
       var select = document.getElementById("years");
@@ -6,10 +24,14 @@ window.addEventListener('load', (event) => {
       option.text = i;
       option.value = i;
   }
+  select.selectedIndex = 4;
+  change_rate_value();
 });
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("number_rate").value;
+    t = document.getElementById("years").value;
+  result = p * (1+(r/100*t));
+  alert(result);
 }
-        
